3b2fcfee9db9edc10921823971797b4db741129c
triniswap <72702451+triniswap@users.noreply.github.com>
2020-12-23 14:00:44 -0400

    wordpress

---
+++ bridge/precache-manifest.f44dce6ada5e7ae8f2df196609eec9e8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4828b270ab9c7557b47a14493957d1b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3a819429e19d7270c23",
+    "url": "/static/js/2.3a8cddd7.chunk.js"
+  },
+  {
+    "revision": "19cdc51015aca30a0cdee13aab1cb231",
+    "url": "/static/js/2.3a8cddd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f0079504aca842bce49",
+    "url": "/static/js/main.8140bf5f.chunk.js"
+  },
+  {
+    "revision": "8f96027f25cd7b7e59d5",
+    "url": "/static/js/runtime-main.db7e2936.js"
+  },
+  {
+    "revision": "3cc78f966af5a6273021893508f9b8bc",
+    "url": "/static/media/confirm-transfer.3cc78f96.svg"
+  },
+  {
+    "revision": "c4e0aaef5cf3287cb0e3f61b010752d0",
+    "url": "/static/media/details.c4e0aaef.svg"
+  },
+  {
+    "revision": "fec20ae271d920f3767a115168e5c537",
+    "url": "/static/media/down-triangle.fec20ae2.svg"
+  },
+  {
+    "revision": "996b4f260a2f5f8b6838a2d796b5ad02",
+    "url": "/static/media/drop-down.996b4f26.svg"
+  },
+  {
+    "revision": "533b267d2e53344de812fac67b67ebf5",
+    "url": "/static/media/loadinglogo.533b267d.png"
+  },
+  {
+    "revision": "ddcbdd689a42d2e324191c3a7ed5136b",
+    "url": "/static/media/search.ddcbdd68.svg"
+  },
+  {
+    "revision": "b8118c6166f7f4af2754569a860c4c3c",
+    "url": "/static/media/transfer.b8118c61.svg"
+  },
+  {
+    "revision": "b638e27fafac090b0fd95c2e127503f6",
+    "url": "/static/media/unlock.b638e27f.svg"
+  },
+  {
+    "revision": "ded82484e6d2b31fa8c242aa23ce33fd",
+    "url": "/static/media/up-triangle.ded82484.svg"
+  }
+]);
