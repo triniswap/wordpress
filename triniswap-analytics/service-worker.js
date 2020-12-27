5350ff786f89ed32d2050ef7dbdc9ac1fc78cc38
triniswap <72702451+triniswap@users.noreply.github.com>
2020-12-27 14:25:59 -0400

    anayltics fix

--- triniswap-analytics/service-worker.js
+++ triniswap-analytics/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e93165d4d5ad8fb31a833412f0e876b4.js"
+  "/precache-manifest.9ed24f06aff2c1f3e49183dc83847b54.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
