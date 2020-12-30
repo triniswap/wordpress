68401d969e06a51f93514ae54cc7e656522a891f
triniswap <72702451+triniswap@users.noreply.github.com>
2020-12-30 10:10:48 -0400

    2020 last update

---
+++ wp-content/themes/quadrum-theme/css/fonts.php
@@ -0,0 +1,16 @@
+
+/* Menu & Titles font family */
+h1, h2, h3,
+h4, h5, h6,
+.header #main-menu a,
+.header #top-sub-menu a,
+.header-topmenu ul li a,
+.header-2-content .header-weather strong,
+.widget-contact li strong,
+.item-block-4 .item-header strong,
+.photo-gallery-grid .item .category-photo {
+	font-family: "Titillium Web", sans-serif;
+}
+
+
+
