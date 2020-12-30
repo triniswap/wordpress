59f90daf304ca7e7cae18fad03686038d60807a6
triniswap <72702451+triniswap@users.noreply.github.com>
2020-12-30 10:28:13 -0400

    2020 last update

---
+++ analytics/precache-manifest.694532b141d343ffd165cc306200e772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1fcdc3a2f38d7be82877d5272ee4a7",
+    "url": "/wordpress/triniswap-analytics/index.html"
+  },
+  {
+    "revision": "db97748a8adf3c8eba05",
+    "url": "/wordpress/triniswap-analytics/static/js/2.6c87a2cc.chunk.js"
+  },
+  {
+    "revision": "69314ac4d35e71d7b27676fee8772f7a",
+    "url": "/wordpress/triniswap-analytics/static/js/2.6c87a2cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c705498cf0d04fc324af",
+    "url": "/wordpress/triniswap-analytics/static/js/main.94a27026.chunk.js"
+  },
+  {
+    "revision": "9a2412632567ef1c35a1",
+    "url": "/wordpress/triniswap-analytics/static/js/runtime-main.b23497ba.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/wordpress/triniswap-analytics/static/media/eth.73dabb37.png"
+  }
+]);
